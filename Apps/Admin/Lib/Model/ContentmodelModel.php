3de64cd491c45001ee7f2ec272e382912ec71237
dingcms <lookcms@gmail.com>
2012-11-24 14:29:56 +0800

    记录操作数据表信息的代码
    记录操作数据表信息的代码

--- Apps/Admin/Lib/Model/ContentmodelModel.php
+++ Apps/Admin/Lib/Model/ContentmodelModel.php
@@ -6,7 +6,32 @@
  * @author Adminsir
  */
 class ContentmodelModel {
-    //put your code here
+
+    //创建数据表
+    function addtable()
+    {
+        $sql = 'CREATE TABLE IF NOT EXISTS `d2d` (
+  `id` int(20) NOT NULL auto_increment,
+  `title_id` int(20) unsigned NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ';
+    }
+    //更新数据表名
+    function edittable(){
+
+    }
+    //删除数据表
+    function droptable(){
+        $sql = 'DROP TABLE d2d';
+    }
+    //增加数据表字段
+    function addfield(){
+        $sql = 'alter table $table add $field $type($length) $null';
+    }
+    //修改数据表字段
+    function editfield(){
+
+    }
 }
 
 ?>
