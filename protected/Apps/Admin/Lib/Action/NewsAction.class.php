2932c0a08871a8f7228dc579f10e5b70bc73d1e6
dingcms <lookcms@gmail.com>
2012-12-15 20:07:50 +0800

    文档列表功能开发（进行中。。。）
    文档列表功能开发（进行中。。。）

--- protected/Apps/Admin/Lib/Action/NewsAction.class.php
+++ protected/Apps/Admin/Lib/Action/NewsAction.class.php
@@ -20,7 +20,19 @@ class NewsAction extends AdminAction {
      * @version dogocms 1.0
      */
     public function index() {
-
+        $m = M('Title');
+        $id = intval($_GET['id']);
+        $id = 1;
+        $condition['sort_id'] = $id;
+        $data = $m->where($condition)->select();
+        $array = array();
+        $array['total'] = 50;
+        $array['rows'] = $data;
+//        echo '<pre>';
+//        echo json_encode($array);
+//        echo '<br/>';
+//        print_r($data);
+//        exit;
         $this->display();
     }
 
@@ -38,12 +50,60 @@ class NewsAction extends AdminAction {
     }
 
     /**
+     * listJsonId
+     * 取得field信息
+     * @access public
+     * @return array
+     * @version dogocms 1.0
+     */
+    public function listJsonId() {
+        $m = M('Title');
+        //$c = M('Title');
+        import('ORG.Util.Page');// 导入分页类
+        $id = intval($_GET['id']);
+        //$id = 1;
+        $pageNumber = intval($_POST['page']);
+       // $pageRows = intval($_POST['rows']);
+        //$pageRows = intval($_GET['rows']);
+        
+        //$pageRows = (intval($_GET['rows'])==FALSE)?(intval($_POST['rows'])):intval($_GET['rows']);
+        if($_POST){
+            //$pageRows = intval($_GET['rows']);
+            $pageRows = intval($_POST['rows']);
+        }else{
+            $pageRows = intval($_GET['rows']);
+        }
+        $pageNumber = (($pageNumber == null || $pageNumber == 0) ? 1:$pageNumber); 
+        $pageRows = (($pageRows == FALSE) ? 20:$pageRows);
+        //$pageRows = 5;
+        //$condition['sort_id'] = array('like','%,'.$id.',%');
+        $condition['sort_id'] = $id;
+        $condition['is_recycle'] = 'false';
+        $count = $m->where($condition)->count();
+        $page       = new Page($count,$pageRows);
+        //$this->dmsg('1', 'dede', true, true);
+        //start = (intPage-1)*number
+        $firstRow = ($pageNumber-1)*$pageRows;
+        //limit($Page->firstRow.','.$Page->listRows)->
+        $data = $m->where($condition)->limit($firstRow.','.$pageRows)->select();
+        //$list = $User->where('status=1')->order('create_time')->limit($page->firstRow.','.$Page->listRows)->select();
+       // $data = $m->where($condition)->limit('1,5')->select();
+        
+        
+        //$data = $m->select();
+        $array = array();
+        $array['total'] = $count;
+        $array['rows'] = $data;
+        
+        echo json_encode($array);
+    }
+    /**
      * json
      * 信息json数据
      * @access public
      * @return array
      * @version dogocms 1.0
-     */
+     
     public function json() {
 
         $m = M('NavHead');
@@ -59,7 +119,7 @@ class NewsAction extends AdminAction {
         echo json_encode($array);
         //echo $json;
     }
-
+*/
     /**
      * jsonCateTree
      * 分类树信息json数据
