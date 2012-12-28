4f6284371fd15f4d2e23b5d741432dd445c2ddf2
dingcms <lookcms@gmail.com>
2012-12-29 00:34:50 +0800

    增加上传代码准备调试

--- protected/Apps/Admin/Lib/Action/UploadAction.class.php
+++ protected/Apps/Admin/Lib/Action/UploadAction.class.php
@@ -19,8 +19,25 @@ class UploadAction extends AdminAction {
      * @return array
      * @version dogocms 1.0
      */
-    public function upload()
-    {
+    public function upload() {
+        echo json_encode(array('error' => 1, 'message' => 'dddddddddddd'));
+        exit;
+        import('ORG.Net.UploadFile');
+        $upload = new UploadFile(); // 实例化上传类
+        $upload->maxSize = 3145728; // 设置附件上传大小
+        $upload->allowExts = array('jpg', 'gif', 'png', 'jpeg'); // 设置附件上传类型
+        $upload->savePath = './Public/Uploads/'; // 设置附件上传目录
+        if (!$upload->upload()) {// 上传错误提示错误信息
+            $this->error($upload->getErrorMsg());
+        } else {// 上传成功 获取上传文件信息
+            $info = $upload->getUploadFileInfo();
+        }
+        // 保存表单数据 包括附件数据
+        $User = M(“User”); // 实例化User对象
+        $User->create(); // 创建数据对象
+        $User->photo = $info[0][“savename”]; // 保存上传的照片根据需要自行组装
+        $User->add(); // 写入用户数据到数据库
+        $this->success(“数据保存成功！”);
         echo json_encode(array('error' => 1, 'message' => 'dddddddddddd'));
         exit;
     }
@@ -32,8 +49,7 @@ class UploadAction extends AdminAction {
      * @return array
      * @version dogocms 1.0
      */
-    public function fileManagerJson()
-    {
+    public function fileManagerJson() {
 
         $php_path = dirname(__FILE__) . '/';
         $php_url = dirname($_SERVER['PHP_SELF']) . '/';
