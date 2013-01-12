d667c54664fdae4a0364e09e7826159c1950b5aa
dingcms <lookcms@gmail.com>
2013-01-12 16:50:22 +0800

    调试自定义模板标签
    调试自定义模板标签

---
+++ protected/Apps/Admin/Common/taglibs.php
@@ -0,0 +1,6 @@
+<?php
+ return array(
+  'blog'=>'@.TagLib.TagLibBlog',  //@.TagLib.TagLibBlog  表示的TagLibBlog的标签位置 "@"表示在当前的项目下；
+                                    //整个的意思是：在当前项目下的TagLib文件夹下的TagLibBlog的文件夹
+     )
+?>
