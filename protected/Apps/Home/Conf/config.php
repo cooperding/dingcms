ae91557ee8f927f872c5f69c66dd4d33289f0b3a
dingcms <lookcms@gmail.com>
2013-01-15 17:01:49 +0800

    themes视图目录调试

--- protected/Apps/Home/Conf/config.php
+++ protected/Apps/Home/Conf/config.php
@@ -1,5 +1,15 @@
 <?php
-return array(
-	//'配置项'=>'配置值'
+
+defined('THINK_PATH') or exit();
+$array = require_once(dirname(THINK_PATH) . '/Config/config.php');
+$miniConfig = array(
+    'ACCESS' => TRUE,
+    'DEFAULT_THEME' => 'default', //默认模板主题名
+        //'TMPL_FILE_DEPR' => '/',//模板文件分隔符
+        //'TMPL_TEMPLATE_SUFFIX' => '.html'
 );
+$array = array_merge($array, $miniConfig);
+//print_r($miniConfig);
+
+return $array;
 ?>
--- protected/Apps/Home/Lib/Action/IndexAction.class.php
+++ protected/Apps/Home/Lib/Action/IndexAction.class.php
@@ -1,7 +1,28 @@
 <?php
-// 本类由系统自动生成，仅供测试用途
-class IndexAction extends Action {
-    public function index(){
-		$this->display();
+
+/**
+ * IndexAction.class.php
+ * 前台首页
+ * 前台核心文件，其他页面需要继承本类方可有效
+ * @author 正侠客 <lookcms@gmail.com>
+ * @copyright 2012- http://www.dingcms.com http://www.dogocms.com All rights reserved.
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @version dogocms 1.0 2012-11-5 11:08
+ * @package  Controller
+ * @todo 完善更多方法
+ */
+class IndexAction extends BaseAction {
+
+    public function index()
+    {
+        //C('DEFAULT_THEME','default');
+        //echo C('DEFAULT_THEME');
+        //exit;
+        //define('DEFAULT_THEME', 'default');
+        $this->assign('url','default');
+        $this->assign('url2','./Tp/default/header.html');
+        $this->display('index');
+        //$this->display();
     }
+
 }
