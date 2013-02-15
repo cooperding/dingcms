10ff9ad738e105b2904aaeabe9d3970f866d9a95
dingcms <lookcms@gmail.com>
2013-02-15 15:33:07 +0800

    部署b2b、b2c及会员结构

---
+++ b2b.php
@@ -0,0 +1,11 @@
+<?php
+//入口文件
+
+//定义项目名称和路径
+define('APP_NAME', 'B2B');
+define('APP_PATH', './protected/Apps/B2B/');
+//开启调试模式
+define('APP_DEBUG',true);
+// 加载框架入口文件
+require("./protected/Core/ThinkPHP.php");
+?>
